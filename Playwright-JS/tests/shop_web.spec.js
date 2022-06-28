6b7c85648e5dafd91c6885a4994ce0d25783c51d
vijayraikar1999 <vijayraikar1999@gmail.com>
2022-06-29 04:14:32 +0530

    Web Scraping

--- Playwright-JS/tests/shop_web.spec.js
+++ Playwright-JS/tests/shop_web.spec.js
@@ -21,11 +21,10 @@ test('Login Playwright test', async ({browser})=> {
     
     // await page.pause();
     
+    await page.pause();
 
     const count = await products.count();
     console.log(count);
-    console.log(count);
-
     for(let i = 0; i < count; i++) {
         const product = await products.nth(i).locator("b").textContent();
         console.log(product);
@@ -38,7 +37,55 @@ test('Login Playwright test', async ({browser})=> {
         }
     }
 
-    await page.pause();
+    await page.locator("[routerlink*='cart']").click();
+    const isThere = page.locator("h3:has-text('zara coat 3')").isVisible();
+    expect(isThere).toBeTruthy();
+
+    await page.locator("text=Checkout").click();
+    await page.locator("[placeholder*='Country']").type("ind", {delay: 100});
+    const dropdown = page.locator(".ta-results");
+    await dropdown.waitFor();
+    const optionCount = await dropdown.locator("button").count();
+    for(let i = 0; i < optionCount; i++) {
+        const text = dropdown.locator("button").nth(i).textContent();
+        if (text === ' India') {
+            // click
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+
+    await expect(page.locator(".user__name input[type='text']")).toHaveText(email);
+    page.locator(".action__submit").click();
+
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    console.log(orderId);
+
+    await page.locator("button[routerlink*='myorders']").click();
+
+    await page.locator("tbody").waitFor();
+
+    const tabelRow = page.locator("tbody tr");
     
+    const tableRowsCount = await page.locator("tbody tr").count();
+
+    for(let i = 0; i < tableRowsCount; i++) {
+        const rowId = tableRow.nth(i).locator("th").textContent();
+        if (rowId === orderId) {
+           await tabelRow.nth(i).locator("button.btn-primary").click();
+           break;
+        }
+    }
+
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
+
+
+
+
+    await page.pause();
+
 } );
 
